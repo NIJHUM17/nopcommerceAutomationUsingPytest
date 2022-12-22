4ba75e607d8e3d6beb927d09abf8a4adbd4acff9
NIJHUM17 <tahsinanijum5199@gmail.com>
2022-12-22 17:20:45 +0600

    nopCommerce Wsbsite Automation  using pytest

--- tests/tests.py
+++ tests/tests.py
@@ -17,7 +17,7 @@ class Test_1:
 #registration
     td = Data()
     testdata = td.data()
-    reg = ast.literal_eval(testdata['DATA']['registration_data'])
+    reg = ast.literal_eval(testdata['TEST_DATA']['registration_data'])
 
     @pytest.mark.order(1)
     def test_registration(self, driver, config, locator):
@@ -31,9 +31,9 @@ class Test_1:
 
 class Test_2:
 #login
-    td = Data()
-    testData = td.data()
-    log = ast.literal_eval(testData['DATA']['login_data'])
+    test_data = Data()
+    testData = test_data.data()
+    log = ast.literal_eval(testData['TEST_DATA']['login_data'])
 
     @pytest.mark.order(2)
     def test_login(self, driver, config, locator):
@@ -47,10 +47,10 @@ class Test_2:
 
 class Test_3:
 #search
-    td = Data()
-    testData = td.data()
-    log = ast.literal_eval(testData['DATA']['login_data'])
-    search = ast.literal_eval(testData['DATA']['search_data'])
+    test_data = Data()
+    testData = test_data.data()
+    log = ast.literal_eval(testData['TEST_DATA']['login_data'])
+    search = ast.literal_eval(testData['TEST_DATA']['search_data'])
 
     @pytest.mark.order(3)
     def test_search(self, driver, config, locator):
@@ -67,31 +67,31 @@ class Test_3:
 
 class Test_4:
 #cart
-    td = Data()
-    testData = td.data()
-    log = ast.literal_eval(testData['DATA']['login_data'])
-    search = ast.literal_eval(testData['DATA']['search_data'])
+    test_data = Data()
+    testData = ttest_data.data()
+    log = ast.literal_eval(testData['TEST_DATA']['login_data'])
+    search = ast.literal_eval(testData['TEST_DATA']['search_data'])
 
     @pytest.mark.order(4)
     def test_shop(self, driver, config, locator):
         open_url = OpenBrowser(driver, config)
         login = Login(driver, locator)
         search = Search(driver, locator)
-        shop_cart = Cart(driver, locator)
+        shoppingcart = Cart(driver, locator)
         open_url.open_webBrowser()
         login.log_in(self.log)
         search.search_box(self.search)
-        shop_cart.shopping_cart()
+        shoppingcart.shopping_cart()
 
 
 
 
 class Test_5:
 #checkout
-    td = Data()
-    testData = td.data()
-    log = ast.literal_eval(testData['DATA']['login_data'])
-    checkOut = ast.literal_eval(testData['DATA']['checkOut_data'])
+    test_data = Data()
+    testData = test_data.data()
+    log = ast.literal_eval(testData['TEST_DATA']['login_data'])
+    checkOut = ast.literal_eval(testData['TEST_DATA']['checkOut_data'])
 
     @pytest.mark.order(5)
     def test_checkout(self, driver, config, locator):
