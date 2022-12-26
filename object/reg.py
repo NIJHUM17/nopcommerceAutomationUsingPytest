df1992e865355ff771b1f8ecdc46129d2a0d55c8
NIJHUM17 <tahsinanijum5199@gmail.com>
2022-12-26 10:47:50 +0600

    nopcommerce Website Automation Using Pytest

--- object/reg.py
+++ object/reg.py
@@ -3,12 +3,12 @@
 class Register:
 
     def __init__(self, driver, locator):
-        self.x = None
+
         self.driver = driver
-        self.reg = locator["LOCATORS"]["registration"]
+        self.registration = locator["LOCATORS"]["registration"]
         self.gender = locator["LOCATORS"]["gender"]
-        self.firstname = locator["LOCATORS"]["first_name"]
-        self.lastname = locator["LOCATORS"]["last_name"]
+        self.first_name = locator["LOCATORS"]["first_name"]
+        self.last_name = locator["LOCATORS"]["last_name"]
         self.birthday = locator["LOCATORS"]["date_of_birth"]
         self.birth_month = locator["LOCATORS"]["date_of_birthmonth"]
         self.birth_year = locator["LOCATORS"]["date_of_birthyear"]
@@ -19,24 +19,22 @@ class Register:
         self.reg_confirm = locator["LOCATORS"]["reg_confirm"]
         self.logout = locator["LOCATORS"]["logout"]
 
-    def registration(self, regwebsite):
-        self.driver.find_element("xpath", self.regwebsite).click()
+    def registration(self, registration):
+        self.driver.find_element("xpath", self.registration).click()
         self.driver.find_element("xpath", self.gender).click()
+        self.driver.find_element("id", self.first_name).send_keys(registration.registration_Data.first_name)
 
+        self.driver.find_element("id", self.last_name).send_keys(registration.registration_Data.last_name)
 
-        self.driver.find_element("id", self.firstname).send_keys(regwebsite.registration_Data.firstname)
+        self.driver.find_element("name", self.birthday).send_keys(registration.registration_Data.date_of_birth)
 
-        self.driver.find_element("id", self.lastname).send_keys(regwebsite.registration_Data.lastname)
+        self.driver.find_element("name", self.birth_month).send_keys(registration.registration_Data.date_of_birthmonth)
 
-        self.driver.find_element("name", self.birthday).send_keys(regwebsite.registration_Data.date_of_birth)
-
-        self.driver.find_element("name", self.birth_month).send_keys(regwebsite.registration_Data.date_of_birthmonth)
-
-        self.driver.find_element("name", self.birth_year).send_keys(regwebsite.registration_Data.date_of_birthyear)
-        self.driver.find_element("id", self.email).send_keys(regwebsite.registration_Data.email)
-        self.driver.find_element("id", self.company).send_keys(regwebsite.registration_Data.company)
-        self.driver.find_element("id", self.password).send_keys(regwebsite.registration_Data.password)
-        self.driver.find_element("id", self.confirm_pass).send_keys(regwebsite.registration_Data.password)
+        self.driver.find_element("name", self.birth_year).send_keys(registration.registration_Data.date_of_birthyear)
+        self.driver.find_element("id", self.email).send_keys(registration.registration_Data.email)
+        self.driver.find_element("id", self.company).send_keys(registration.registration_Data.company)
+        self.driver.find_element("id", self.password).send_keys(registration.registration_Data.password)
+        self.driver.find_element("id", self.confirm_pass).send_keys(registration.registration_Data.password)
         self.driver.find_element("xpath", self.reg_confirm).click()
         self.driver.find_element("xpath", self.logout).click()
 
